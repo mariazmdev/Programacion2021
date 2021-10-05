81d8ecf63690a6ea837d715049b5f638408c6670
María Zawadzki <83785077+mariazmdev@users.noreply.github.com>
2021-10-05 19:34:59 +0200

    Create Ej1.java

---
+++ Repaso2021/Nivel_4/Ej1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Nivel_4;
+
+/**
+ *
+ * @author maria
+ */
+public class Ej1 {
+    
+    public static void main(String [] args){
+        
+        for(int i = 1; i <=20; i++){
+            System.out.println(i);
+        }
+        
+        
+    }
+    
+}
