b423133b6995d8b909893a069b72726860558fa8
María Zawadzki <83785077+mariazmdev@users.noreply.github.com>
2021-10-08 19:38:29 +0200

    Update DibujarRectangulo.java

--- PrimerTrimestre/DibujarRectangulo.java
+++ PrimerTrimestre/DibujarRectangulo.java
@@ -31,25 +31,27 @@ public class DibujarRectangulo {
             
             if(opcion < 0 || opcion > 2){
                 System.out.println("*Opción errónea. Introduzca una opción de nuevo.*");
-            } else if (opcion != 0){
-                
-                int base = introducirBase();
-                int altura = introducirAltura();
+            } else if (opcion != 0){                  
                 
                 System.out.println("\n");
                 
                 switch (opcion) {
                     case 1:
                         System.out.println("");
+                        rectangulo();
                         break;
                     
+                    case 2:
+                        System.out.println("");
+                        triangulo();
+                        break;
                 }
                 
             }
             
-        } while (opcion != 3);
+        } while (opcion != 0);
         
-        
+        System.out.println("***Finalizando la ejecución del programa***");
         
     }
     
@@ -60,23 +62,48 @@ public class DibujarRectangulo {
         System.out.println("***** DIBUJANDO FIGURAS *****");
         System.out.println("1. Dibujar rectángulo");
         System.out.println("2. Dibujar triángulo");
-        System.out.println("3. Salir");
+        System.out.println("0. Salir");
+        System.out.println("");
+        System.out.println("**Introduce una opción válida: ");
         
         
     }
     
-    public static int introducirBase() { 
+    public static void rectangulo() {
+        
         System.out.println("Introduce la base: ");
-        int num = scan.nextInt();
-        return num;
+        int base = scan.nextInt();      
+        
+        System.out.println("Introduce la altura: ");
+        int altura = scan.nextInt();
+        System.out.println("");
+        
+        for (int i = 0; i < altura; i++){
+            
+            for(int j = 0; j < base; j++){
+                System.out.print("* ");                
+            }
+                        
+            System.out.println("");
+            
+        }        
         
     }
     
-    public static int introducirAltura() { 
-        System.out.println("Introduce la altura: ");
-        int num = scan.nextInt();
-        return num;
+    
+    public static void triangulo(){
+        System.out.println("Introduce la altura del triángulo: ");
+        int altura = scan.nextInt();
+        System.out.println("");
         
+        for (int i = 1; i <= altura; i++) {
+            
+            for (int j = 1; j <= i; j++) {                
+                System.out.print("* ");
+            }
+
+            System.out.println("");
+        }      
     }
     
 }
