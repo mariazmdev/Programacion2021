89998eaa9fdf888fef6443a329f3a56fb47a5ebd
María Zawadzki <83785077+mariazmdev@users.noreply.github.com>
2021-11-22 19:11:12 +0100

    Update NotasAlumnosMain.java

--- PrimerTrimestre/NotasAlumnosPOO/NotasAlumnosMain.java
+++ PrimerTrimestre/NotasAlumnosPOO/NotasAlumnosMain.java
@@ -15,7 +15,11 @@ public class NotasAlumnosMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Alumno alumno = new Alumno("Sandra","González","Díaz");
         
+        int[][] notas = {{5,4,6,0},{8,5,7,0},{1,2,3,0},{7,8,9,0},{5,4,6,0},{8,5,7,0}};
+        
+        alumno.setNotas(notas);
         
     }
     
